2dcecfad485b7beafebe9f857fc0d1b27aa17a1c
abez <abram.hindle@softwareprocess.us>
2010-02-16 22:52:14 -0500

    first attempt to make random-schedule permuter

--- permuter.ml
+++ permuter.ml
@@ -147,68 +147,102 @@ let calculate_color_distance  blocksize input_colors palette_colors matrix =
   matrix
 ;;      
 
-let permuter input_image blocksize start len =
+let fill_image_with_int_array img iarr =
+  let len = Array.length iarr in
+  for i = 0 to len - 1 do
+    img#unsafe_set (i mod dwidth) (i / dwidth) (rgb_of_int iarr.(i))
+  done;
+    img
+;;
+
+let get_colors_via_schedule_arr img schedule start len arr =
+  for i = 0 to len - 1 do
+    let xy = schedule.(start + i) in
+    let x = xy mod dwidth in
+    let y = xy / dwidth in
+    arr.(i) <- img#get x y
+  done;
+  arr
+;;
+
+
+let scatter_permuter input_image blocksize =
+  let start = 0 in
+  let len =  dwidth in
   assert(blocksize <= dwidth);
   assert(dwidth mod blocksize = 0);
   assert(len > 0 && len <= dwidth);
+  (* this is for this branch 
+     we only operate on full size images
+  *)
+  assert(len = dwidth);
+  assert(start = 0);
   status ("Loading the image "^ input_image);
   let img = load_rgb_file input_image in
     assert(img#width = dwidth);
     assert(img#height = dwidth);
     let n = dwidth / blocksize in
-    let output_file_name = input_image ^ "." ^ (string_of_int blocksize) ^"."^ (string_of_int start) ^ "." ^ (string_of_int len) ^ ".png" in
-      status ("Output file: "^output_file_name);
-
-    let oimg = new rgb24 dwidth len in
+    
     let colors = Array.create dwidth {r = 0 ; g =0 ; b =0} in
-    let bcolors =  Array.create blocksize { r = 0; g = 0; b = 0 } in
+    let bcolors     = Array.create blocksize { r = 0; g = 0; b = 0 } in
     let line_colors = Array.create blocksize { r = 0; g = 0; b = 0 } in
+    (* this initial palette *)
     let palette = random_palette () in
-    let distance_matrix = Array.make_matrix blocksize blocksize 0.0 in
-      for i = start to (start + len - 1) do
-        status ("Line "^(string_of_int i));
-        status ("Get that line "^(string_of_int i));
-        let line = img_line img i colors in
-          for j = 0 to (n - 1) do
-            (* status ("Block "^(string_of_int j)); *)
-            let palette_colors = get_colors_for_arr palette blocksize ((i*n) + j) bcolors in
-            let line_colors = get_colors_of_line_arr line blocksize j line_colors in
+    let random_schedule = random_palette in (* heheh *)
+    let noise_img = 
+      let oimg = new rgb24 dwidth len in
+        fill_image_with_int_array oimg palette 
+    in
+
+    (* try to make oimg closer to img *)
+    let random_permute_step stepn img oimg =
+      let output_file_name = input_image ^ "." ^ (string_of_int blocksize) ^"."^ (string_of_int start) ^ "." ^ (string_of_int len) ^ "." ^ (string_of_int stepn)  ^".png" in
+        status ("Output file: "^output_file_name);
+        let schedule = random_schedule () in (* see I'm saying what I mean! *)
+        let distance_matrix = Array.make_matrix blocksize blocksize 0.0 in (* this will be mutated *)
+        let n = drange / blocksize in
+        for i = 0 to n - 1 do
+          let palette_colors = get_colors_via_schedule_arr oimg schedule (i*blocksize) blocksize bcolors in
+          let line_colors    = get_colors_via_schedule_arr img  schedule (i*blocksize) blocksize line_colors in
+            status ("Block #" ^ (string_of_int n));
             let distance_matrix = calculate_color_distance blocksize line_colors palette_colors distance_matrix in
             let mask =   munkres distance_matrix in
             let (p1,p2) = find_stars mask in
               for k = 0 to blocksize - 1 do
-              (* ok now we we take the mappings from p1 and find which colors best match *)
-              (* I hope this isn't backwards *)
+                (* ok now we we take the mappings from p1 and find which colors best match *)
+                (* I hope this isn't backwards *)
                 oimg#unsafe_set ((blocksize * j) + k) (i - start) (palette_colors.(p1.(k)))
               done;
-          done;
-      done;
-        oimg#save output_file_name (Some Png) [];
+        done;
+          status ("Saving: "^output_file_name);
+          oimg#save output_file_name (Some Png) [];
+          oimg
+    in
+
+    let rec helper n img oimg = function
+        i when i = n -> oimg
+      | i when i < n -> 
+          helper n img (random_permute_step i img oimg) (i + 1)
+    in
+
+      helper 30 img noise_img 0
+
+      
 
 ;;
 
-let dispatch_from_cmdline () =
+let sp_dispatch_from_cmdline () =
   Random.init 666;
   let files = ref [] in
     Arg.parse [] (fun s -> files := s :: !files) "edge files";
     let files = List.rev !files in
       match files with
           [] -> prerr_endline "./permuter <input_image> <blocksize> <startline> <nlines>"
-        | (filename::[]) -> permuter filename  128 0 dwidth
-        | (filename::blocksize::[]) -> permuter filename (int_of_string blocksize) 0 dwidth
-        | (filename::blocksize::start::[]) ->
-            let blocksize = (int_of_string blocksize) in
-            let start = (int_of_string start) in
-            let len   = dwidth - start in
-              permuter (List.hd files) blocksize start len
-        | (filename::blocksize::start::len::xs) ->
-            let blocksize = (int_of_string blocksize) in
-            let start = (int_of_string start) in
-            let len   = (int_of_string len) in
-              permuter (List.hd files) blocksize start len
+        | (filename::[]) -> scatter_permuter filename  128 
+        | (filename::blocksize::xs) -> scatter_permuter filename (int_of_string blocksize) 
         
 ;;
   
 
-dispatch_from_cmdline ();;         
+sp_dispatch_from_cmdline ();;         
       
